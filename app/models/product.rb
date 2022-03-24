c963654a844e2a41e4e3b6541b00edec06784e52
HeRoYo <11bykko11@gmail.com>
2022-03-24 19:36:47 +0900

    バリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,4 @@
 class Product < ApplicationRecord
-
-  belongs_to :user
-  has_one_attached :image
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
@@ -10,11 +6,20 @@ class Product < ApplicationRecord
   belongs_to :delivery_source
   belongs_to :preparation_period
 
-  with_options presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true, numericality: { other_than: 1 , message: "can't be blank"} do
     validates :category_id
     validates :condition_id
     validates :delivery_change_id
     validates :delivery_source_id
     validates :preparation_period_id
   end
+
+  validates :image,         presence: true
+  validates :products_name, presence: true
+  validates :description,   presence: true
+
+  validates :price,         presence: true, format: { with: /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid" }
 end
