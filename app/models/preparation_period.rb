890d05bc1006a9c8f0178a6f77dbe8fcddc32431
HeRoYo <11bykko11@gmail.com>
2022-03-24 19:10:59 +0900

    ActiveHashの導入

---
+++ app/models/preparation_period.rb
@@ -0,0 +1,11 @@
+class Preparation_preiod < ActiveHash::Base
+  self.date = [
+    { id: 1, name: '--' },
+    { id: 2, name: '4~7日で発送' },
+    { id: 3, name: '12日で発送' },
+    { id: 4, name: '23日で発送' },
+  ]
+
+  include ActiveHash::Associations
+  has_many :products
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,19 @@ class Product < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_change
+  belongs_to :delivery_source
+  belongs_to :preparation_period
+
+  with_options presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :category_id
+    validates :condition_id
+    validates :delivery_change_id
+    validates :delivery_source_id
+    validates :preparation_period_id
+  end
 end
