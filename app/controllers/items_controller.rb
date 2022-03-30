13b0e5596a0f3aeaddd53a066b739ccbd09e1105
HeRoYo <11bykko11@gmail.com>
2022-03-31 08:48:28 +0900

    出品者のみ編集ページへ移動できる設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :back_to_top, only: :edit
 
   def index
     @products = Product.all.order("created_at DESC")
@@ -40,6 +41,13 @@ class ItemsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:image, :products_name, :description, :category_id, :condition_id, :delivery_change_id, :delivery_source_id, :preparation_period_id, :price).merge(user_id: current_user.id)
+  end
+
+  def back_to_top
+    @product = Product.find(params[:id])
+    unless current_user == @product.user
+      redirect_to root_path
+    end
   end
 
 end
