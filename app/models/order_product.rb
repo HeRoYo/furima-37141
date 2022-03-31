0ab71396c5b695adbcd0d73626f564e91c0ca8c5
HeRoYo <11bykko11@gmail.com>
2022-03-31 16:50:31 +0900

    データを保存する処理

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,6 +1,6 @@
 class OrderProduct
   include ActiveModel::Model
-  
+
   attr_accsessor :user, :product, :post_code, :prefecture_id, :city, :house_number, :building, :phone_number
 
   with_option presence: true do
@@ -14,5 +14,13 @@ class OrderProduct
   end
 
   validates :building
+
+  def save
+    #購入品の情報
+    purchase = Purchase.create(user: user, product: product)
+    
+    #配送先の情報
+    ShippingAddress.create(post_code: post_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, phone_number: phone_number, purchase_id: purchase.id)
+  end
 
 end
