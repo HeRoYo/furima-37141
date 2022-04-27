8c1c1f224a9d22e2a93996bab780985e9ebb6c2c
HeRoYo <11bykko11@gmail.com>
2022-04-28 04:34:11 +0900

    コメントチャンネル編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to product_path(params[:product_id])
+      ActionCable.server.broadcast "comment_channel", {comment: @comment, user: @comment.user}
     end
   end
 
