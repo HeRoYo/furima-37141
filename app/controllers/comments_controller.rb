fc4af4273a3cb35f064e5918e0030e6714622d36
HeRoYo <11bykko11@gmail.com>
2022-04-28 05:07:28 +0900

    他の商品へのコメントが表示されないように編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
+    @product = Product.find(params[:product_id])
     if @comment.save
-      ActionCable.server.broadcast "comment_channel", {comment: @comment, user: @comment.user}
+      CommentChannel.broadcast_to @product, { comment: @comment, user: @comment.user }
     end
   end
 
--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -2,7 +2,11 @@ import consumer from "./consumer"
 
 if(location.pathname.match(/\/products\/\d/)){
 
-  consumer.subscriptions.create("CommentChannel", {
+  consumer.subscriptions.create({
+    channel: "CommentChannel",
+    product_id: location.pathname.match(/\d+/)[0]
+  }, {
+
     connected() {
       // Called when the subscription is ready for use on the server
     },
